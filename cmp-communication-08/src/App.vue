9015ac92ecf593ccd17ad60a113f61b3a0763399
Prashant <prashant@inuvest.tech>
2023-01-13 14:50:19 +0530

    component-communication-parent-child

--- cmp-communication-08/src/App.vue
+++ cmp-communication-08/src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base></knowledge-base>
   </div>
 </template>
 
@@ -31,6 +31,12 @@ export default {
       ],
       activeTopic: null,
     };
+  },
+  provide(){
+    return{
+      topics: this.topics,
+      selectTopic:this.activateTopic
+    }
   },
   methods: {
     activateTopic(topicId) {
